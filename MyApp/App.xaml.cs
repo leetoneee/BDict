905127e0fa7fc90926364eb34b88904a15b480a7
leetoneee <letoan7442@gmail.com>
2023-11-12 09:01:00 +0700

    Add project files.

---
+++ MyApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MyApp.MVVM.Views;
+
+namespace MyApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MyApp/MVVM/Views/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MyApp.MVVM.Views;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
