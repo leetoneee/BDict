9f6e33653522a3d53efcf2670e04fc7f134e07b0
leetoneee <letoan7442@gmail.com>
2023-11-26 22:39:03 +0700

    add SearchView complete

---
+++ MyApp/MVVM/ViewModels/SearchViewModel.cs
@@ -0,0 +1,33 @@
+﻿using MyApp.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.MVVM.ViewModels
+{
+    public class SearchViewModel
+    {
+        public ObservableCollection<Vocabulary> RecentSearches { get; set; }
+
+        public SearchViewModel() {
+            RecentSearches = new ObservableCollection<Vocabulary>()
+            {
+                new Vocabulary{
+                    Word = "Hello"
+                },
+                new Vocabulary{
+                    Word = "Machine"
+                },
+                new Vocabulary{
+                    Word = "Good"
+                },
+                new Vocabulary{
+                    Word = "Build"
+                },
+            };
+        }
+    }
+}
