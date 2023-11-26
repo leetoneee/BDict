b1f57f3db32b3e38b07116e8729323000300d321
Dolh <22520254@gm.uit.edu.vn>
2023-11-26 16:08:04 +0700

    Xong co ban TranslatorView

--- MyApp/MVVM/Views/TranslatorView.xaml.cs
+++ MyApp/MVVM/Views/TranslatorView.xaml.cs
@@ -6,4 +6,9 @@ public partial class TranslatorView : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ImageButton_Clicked(object sender, EventArgs e)
+    {
+		DisplayAlert("Alert", "copy click", "ok");
+    }
 }
