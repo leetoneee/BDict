4a33bee0cbe024e389d5a04a780b77f0a34b9f8f
Dolh <22520254@gm.uit.edu.vn>
2023-11-27 13:28:41 +0700

    Chinh sua UI TransView

--- MyApp/MVVM/Views/TranslatorView.xaml.cs
+++ MyApp/MVVM/Views/TranslatorView.xaml.cs
@@ -11,4 +11,15 @@ public partial class TranslatorView : ContentPage
     {
 		DisplayAlert("Alert", "copy click", "ok");
     }
+
+    private void twoArrowBtn_Clicked(object sender, EventArgs e)
+    {
+        string temp1 = upLabel.Text;
+        upLabel.Text = downLabel.Text;
+        downLabel.Text = temp1;
+
+        string temp2 = firstBtn.Text;
+        firstBtn.Text = secondBtn.Text;
+        secondBtn.Text = temp2;
+    }
 }
