a71110ab7e61fcc8970ed573d1394ecf1e45bdc6
Dự Đoàn Danh <22520260@ms.uit.edu.vn>
2023-11-29 22:11:01 +0700

    Add BookmarkView

---
+++ MyApp/MVVM/ViewModels/BookmarkViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+using MyApp.MVVM.Views;
+
+namespace MyApp.MVVM.ViewModels
+{
+    public class BookmarkViewModel
+    {
+        public ICommand AddCommand { get; }
+
+        public BookmarkViewModel()
+        {
+            AddCommand = new Command(AddBtn_Clicked);
+        }
+
+        public async void AddBtn_Clicked()
+        {
+            // Lấy tham chiếu đến trang hiện tại của TabbedPage
+            var tabbedPage = (TabbedPage)Application.Current.MainPage;
+
+            // Tạo trang mới
+            var searchView = new SearchView();
+
+            // Thiết lập trang mới làm trang hiện tại
+            tabbedPage.CurrentPage = new NavigationPage(searchView);
+        }
+    }
+}
--- MyApp/MVVM/Views/BookmarkView.xaml.cs
+++ MyApp/MVVM/Views/BookmarkView.xaml.cs
@@ -1,3 +1,5 @@
+using MyApp.MVVM.ViewModels;
+
 namespace MyApp.MVVM.Views;
 
 public partial class BookmarkView : ContentPage
@@ -5,5 +7,6 @@ public partial class BookmarkView : ContentPage
 	public BookmarkView()
 	{
 		InitializeComponent();
+		BindingContext = new BookmarkViewModel();
 	}
 }
