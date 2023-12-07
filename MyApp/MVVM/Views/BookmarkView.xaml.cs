dcbe10e62f1e873c1ea55317f3e3d6eb89cf8f03
leetoneee <letoan7442@gmail.com>
2023-12-07 17:56:30 +0700

    fix bookmark

--- MyApp/MVVM/Views/BookmarkView.xaml.cs
+++ MyApp/MVVM/Views/BookmarkView.xaml.cs
@@ -1,12 +1,20 @@
+using MyApp.MVVM.Models;
 using MyApp.MVVM.ViewModels;
 
 namespace MyApp.MVVM.Views;
 
 public partial class BookmarkView : ContentPage
 {
-	public BookmarkView()
-	{
-		InitializeComponent();
-		BindingContext = new BookmarkViewModel();
-	}
+    public BookmarkView()
+    {
+        InitializeComponent();
+        BindingContext = new BookmarkViewModel();
+        this.Appearing += OnBookmarkViewAppearing;
+    }
+
+    private void OnBookmarkViewAppearing(object sender, EventArgs e)
+    {
+        InitializeComponent();
+        BindingContext = new BookmarkViewModel();
+    }
 }
