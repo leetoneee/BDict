3147484ed55e6e5b94cc39e21811b0cc19ba25aa
leetoneee <letoan7442@gmail.com>
2023-12-24 10:18:13 +0700

    mvvm checkUpdate

--- MyApp/MVVM/Views/InfoView.xaml.cs
+++ MyApp/MVVM/Views/InfoView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using MyApp.MVVM.ViewModels;
+using System.Net.Http;
 using System.Net.Http.Headers;
 
 namespace MyApp.MVVM.Views;
@@ -8,61 +9,6 @@ public partial class InfoView : ContentPage
     public InfoView()
 	{
 		InitializeComponent();
+		BindingContext = new InfoViewModel();
 	}
-
-    private void OnHomepageLabelTapped(object sender, EventArgs e)
-    {
-        Launcher.OpenAsync(new Uri("https://lehuudouit.github.io/WebBDict/"));
-    }
-
-    private void updateBtn_Clicked(object sender, EventArgs e)
-    {
-        var currentNetwork = Connectivity.NetworkAccess;
-
-        if (currentNetwork == NetworkAccess.Internet)
-        {
-            // Nếu có mạng, thực hiện yêu cầu API
-            _ = FetchAPI();
-        }
-        else
-        {
-            // Hiển thị thông báo nếu không có mạng
-            DisplayAlert("Network Error", "No internet connection available", "OK");
-        }
-    }
-
-    public async Task FetchAPI()
-    {
-        string url = "https://gist.githubusercontent.com/lehuudoUIT/2f01b66c39aba9dc61d158a302f6be4f/raw";
-
-        try
-        {
-            using (HttpClient httpClient = new HttpClient())
-            {
-                httpClient.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue { NoCache = true, NoStore = true };
-                
-                string response = await httpClient.GetStringAsync(url);
-                await Console.Out.WriteLineAsync(response);
-
-                string currentVersion = AppInfo.VersionString;
-                await Console.Out.WriteLineAsync(currentVersion);
-
-                if (response == currentVersion)
-                {
-                    await DisplayAlert("Update Information", "Your app is the latest version", "OK");
-                }
-                else
-                {
-                    await DisplayAlert("Update Information", "Your app is not the latest version. Check for update on our website", "OK");
-                }
-                httpClient.Dispose();
-            }
-        }
-        catch (HttpRequestException)
-        {
-            await DisplayAlert("Update Information", "Network or server have problems", "OK");
-            await DisplayAlert("Update Information", "Server have problems", "OK");
-            return;
-        }
-    }
 }
