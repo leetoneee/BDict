5468bf52616c97eeaa2701cfd1ae81ed734aabf4
Dự Đoàn Danh <22520260@ms.uit.edu.vn>
2023-12-25 17:41:42 +0700

    Info and TranslatorView

--- MyApp/MVVM/Views/InfoView.xaml.cs
+++ MyApp/MVVM/Views/InfoView.xaml.cs
@@ -10,6 +10,27 @@ public partial class InfoView : ContentPage
 
     private void OnHomepageLabelTapped(object sender, EventArgs e)
     {
-        Launcher.OpenAsync(new Uri("https://lehuudouit.github.io/WebBDict/"));
+        var uri = new Uri("https://lehuudouit.github.io/WebBDict/");
+        Launcher.OpenAsync(uri);
     }
+
+
+    private async void OnSendButtonClicked(object sender, EventArgs e)
+    {
+        var subject = "Feedback";
+        var body = feedbackEditor.Text; // Assuming feedbackEditor is the Editor control for user input
+
+        var uri = new Uri($"mailto:bdictdictionary@gmail.com?subject={Uri.EscapeDataString(subject)}&body={Uri.EscapeDataString(body)}");
+
+        try
+        {
+            await Launcher.OpenAsync(uri);
+            await DisplayAlert("Success", "Feedback sent successfully!", "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"An error occurred: {ex.Message}", "OK");
+        }
+    }
+
 }
